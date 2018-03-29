47a547b570685b2561d95fb1fe2b81289102fa7f
jalbatross <joeycalbano@gmail.com>
2018-03-29 15:35:39 -0700

    Working pt2 of tutorial!

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+import Media from 'react-media'
 import './index.css'
 import "../styles/layout-override.css";
 
@@ -11,20 +13,66 @@ const TemplateWrapper = ({ children }) => (
     <Helmet
       title="Gatsby Default Starter"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
       ]}
     />
     <Header />
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        margin: "0 auto",
+        maxWidth: 980,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "100%"
       }}
     >
-      {children()}
+      <Media query={{ maxWidth: 848 }}>
+        {matches =>
+          matches ? (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 1 }}>{children()}</div>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 2.5, paddingRight: "30px" }}>
+                {children()}
+              </div>
+<div style={{ flex: 1 }}>
+                <Sidebar
+                  title="Codestack"
+                  description="Articles on React and Node.js. All articles are written by Me. Fullstack Web Development."
+                />
+                <Sidebar
+                  title="About author"
+                  description="I am Joey!"
+                />
+              </div>
+            </div>
+          )
+        }
+      </Media>
     </div>
   </div>
 )
