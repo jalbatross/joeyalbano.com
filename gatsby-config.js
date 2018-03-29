3052f00c184744f8854673cafdf2bc9f49b8d8ad
jalbatross <joeycalbano@gmail.com>
2018-03-29 15:28:07 -0700

    first stable commit for blog

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: ['gatsby-plugin-react-helmet',
+            'gatsby-plugin-catch-links',
+            {
+               resolve:'gatsby-source-filesystem',
+               options: {
+                  path: '/home/joey/Documents/Gits/myBlog/src/pages',
+                  name: 'pages',
+               },
+            },
+            'gatsby-transformer-remark',
+         ],
+};
