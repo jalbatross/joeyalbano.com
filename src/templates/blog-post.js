c57dcd4ca45907ab92f9e42c2a61da3545bbe553
jalbatross <ih8u2o@gmail.com>
2018-10-22 09:51:16 -0700

    Tag comma separation in blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,14 +6,13 @@ export default function Template({
   data 
 }) {
   const post = data.markdownRemark; 
-  console.log(post.frontmatter);
   return (
     <div className="blog-post-container">
      <Helmet title={`Jlife - ${post.frontmatter.title}`} />
       <div className="blog-post">
         <h1>{post.frontmatter.title}</h1>
         <h2>{post.frontmatter.date}</h2>
-        <h2>Tags: {post.frontmatter.tags.toString()}</h2>
+        <h2>Tags: {post.frontmatter.tags.map((x, index, arr)=> index !== arr.length - 1 ? x+', ' : x)}</h2>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: post.html }}
